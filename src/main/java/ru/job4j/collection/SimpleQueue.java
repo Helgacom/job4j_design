373117db74b40929cdea985f48a660f827bd727d
Olga Remneva <remneva_olga@mail.ru>
2023-03-29 15:41:16 +0400

    a queue on two stacks - fixed

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,15 +1,20 @@
 package ru.job4j.collection;
 
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
 
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    int sizeIn = 0;
-    int sizeOut = 0;
+    private int sizeIn = 0;
+    private int sizeOut = 0;
 
 
     public T poll() {
+        if (sizeIn == 0 && sizeOut == 0) {
+            throw new NoSuchElementException();
+        }
         if (sizeOut == 0) {
             while (sizeIn != 0) {
                 out.push(in.pop());
