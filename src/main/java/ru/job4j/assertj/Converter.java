07c712de6c1e970f8a1e31bd7ea119e232976ba8
Olga Remneva <remneva_olga@mail.ru>
2023-06-10 20:23:41 +0400

    Converter for sorted array

--- src/main/java/ru/job4j/assertj/Converter.java
+++ src/main/java/ru/job4j/assertj/Converter.java
@@ -2,10 +2,19 @@ package ru.job4j.assertj;
 
 public class Converter {
 
-    public static int[] convert(int[] data) {
-        for (int i = 0; i < data.length; i++) {
-            data[i] = data[i] * data[i];
+    public static int[] convertSortedArray(int[] data) {
+        int[] rsl = new int[data.length];
+        int left = 0;
+        int right = data.length - 1;
+        for (int i = data.length - 1; i >= 0; i--) {
+            if (Math.abs(data[left]) < Math.abs(data[right])) {
+                rsl[i] = data[right] * data[right];
+                right--;
+            } else {
+                rsl[i] = data[left] * data[left];
+                left++;
+            }
         }
-        return data;
+        return rsl;
     }
 }
--- src/test/java/ru/job4j/assertj/ConverterTest.java
+++ src/test/java/ru/job4j/assertj/ConverterTest.java
@@ -3,16 +3,15 @@ package ru.job4j.assertj;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static ru.job4j.assertj.Converter.convert;
+import static ru.job4j.assertj.Converter.convertSortedArray;
 
 class ConverterTest {
 
     @Test
     void checkConvert() {
-        int[] nums = new int[]{-4, 7, 0, 9, 3, -8, 9};
-        int[] rsl = convert(nums);
-        assertThat(rsl).hasSize(7)
-                .containsExactly(16, 49, 0, 81, 9, 64, 81);
+        int[] nums = new int[]{-14, -7, -2, 0, 9, 13, 18, 29};
+        int[] rsl = convertSortedArray(nums);
+        assertThat(rsl).hasSize(8)
+                .containsExactly(0, 4, 49, 81, 169, 196, 324, 841);
     }
-
 }
