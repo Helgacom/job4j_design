248466d5bcf98510b3aa61f152f56e00ac544f8b
Olga Remneva <remneva_olga@mail.ru>
2023-06-30 23:50:16 +0400

    SimpleFileVisitor - duplicates search - fixed

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -12,26 +12,25 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
     private final Map<FileProperty, List<Path>> map = new HashMap<>();
 
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        var fileProperty = new FileProperty(attrs.size(), file.toFile().getName());
-        if (!map.containsKey(fileProperty)) {
-            map.put(fileProperty, new LinkedList<>());
-            map.get(fileProperty).add(file.toAbsolutePath());
-        } else {
-            map.get(fileProperty).add(file.toAbsolutePath());
-        }
-        Set<Path> rsl;
-        rsl = map.keySet().stream()
-                        .filter(f -> f.equals(fileProperty) && map.get(f).size() > 1)
-                        .flatMap(f -> map.get(f).stream())
-                        .collect(Collectors.toSet());
+    public void printRsl(Set<Path> rsl, FileProperty fileProperty) {
         if (rsl.size() > 0) {
             System.out.println(fileProperty.getName() + " ; " + fileProperty.getSize());
             for (Path f : rsl) {
                 System.out.println(f);
             }
         }
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        var fileProperty = new FileProperty(attrs.size(), file.toFile().getName());
+        map.putIfAbsent(fileProperty, new LinkedList<>());
+        map.get(fileProperty).add(file.toAbsolutePath());
+        Set<Path> rsl = map.keySet().stream()
+                        .filter(f -> f.equals(fileProperty) && map.get(f).size() > 1)
+                        .flatMap(f -> map.get(f).stream())
+                        .collect(Collectors.toSet());
+        printRsl(rsl, fileProperty);
         return super.visitFile(file, attrs);
     }
 }
