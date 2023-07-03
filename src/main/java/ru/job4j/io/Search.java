7d1bbb152be9bf20704b9333112c9d00ecb40e75
Olga Remneva <remneva_olga@mail.ru>
2023-07-03 17:19:09 +0400

    startup parameters validation

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,12 +9,23 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static boolean validate(Path path) {
+        if (!path.startsWith(".")) {
+            throw new IllegalArgumentException("Wrong start folder, use root folder");
+        }
+        if (path.getFileName().toString().split(".").length == 2) {
+            throw new IllegalArgumentException("Wrong folder format");
+        }
+        return true;
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
+        validate(root);
         Files.walkFileTree(root, searcher);
         return searcher.getLines();
     }
