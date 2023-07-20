eb3956028ca0b3c9733ba917582a31ffa8999835
Olga Remneva <remneva_olga@mail.ru>
2023-07-20 17:15:55 +0400

    Slf4j - variables output

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        byte a = 1;
+        short s = 1;
+        char c = '1';
+        int i = 1;
+        long l = 1L;
+        float f = 1.0f;
+        double d = 1.0;
+        boolean b = true;
+        LOG.debug("Variable list: {}, {}, {}, {}, {}, {}, {}, {}", a, s, c, i, l, f, d, b);
+
     }
 }
