a333ad8c65a04c0aa341cff6f8cdb20a9aca2e81
Olga Remneva <remneva_olga@mail.ru>
2023-08-11 11:50:42 +0400

    select - fixed

--- data/db/fauna.sql
+++ data/db/fauna.sql
@@ -1,26 +1,26 @@
-CREATETABLE fauna (
+create table fauna (
     id serial primary key,
     name text,
     avg_age int,
     discovery_date date
 );
 
-INSERT INTO fauna(name, avg_age, discovery_date)
-VALUES ('tiger', 7080, '1000-01-01');
-INSERT INTO fauna(name, avg_age, discovery_date)
-VALUES ('stork', 2478, '1111-01-01');
-INSERT INTO fauna(name, avg_age, discovery_date)
-VALUES ('python', 4248, '1201-01-01');
-INSERT INTO fauna(name, avg_age, discovery_date)
-VALUES ('giraffe', 10620, '1302-01-01');
-INSERT INTO fauna(name, avg_age, discovery_date)
-VALUES ('elefant', 21240, '1010-01-01');
-INSERT INTO fauna(name, avg_age, discovery_date)
-VALUES ('kangaroos', 12980, '1770-01-01');
-INSERT INTO fauna(name, avg_age)
-VALUES ('cat', 5310);
-SELECT* FROM fauna;
-SELECT* FROM fauna WHERE name LIKE '%fish%';
-SELECT* FROM fauna WHERE avg_age > 10000 AND avg_age < 21000;
-SELECT* FROM fauna WHERE discovery_date IS NULL;
-SELECT* FROM fauna WHERE discovery_date < '1950-01-01';
+insert into fauna(name, avg_age, discovery_date)
+values ('tiger', 7080, '1000-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('stork', 2478, '1111-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('python', 4248, '1201-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('giraffe', 10620, '1302-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('elefant', 21240, '1010-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('kangaroos', 12980, '1770-01-01');
+insert into fauna(name, avg_age)
+values ('cat', 5310);
+select * from fauna;
+select * from fauna where name like '%fish%';
+select * from fauna where avg_age > 10000 and avg_age < 21000;
+select * from fauna where discovery_date is null;
+select * from fauna where discovery_date < '1950-01-01';
