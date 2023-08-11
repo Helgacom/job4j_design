172a60b97020576799740f6fef2416f5e90c1bc7
Olga Remneva <remneva_olga@mail.ru>
2023-08-11 19:07:05 +0400

    scheme of user rights and roles - fixed 2

--- data/db/ItemsSystem/create.sql
+++ data/db/ItemsSystem/create.sql
@@ -1,12 +1,12 @@
+create table roles(
+	id serial primary key,
+	role_name varchar(255)
+);
+
 create table users(
 	id serial primary key,
 	name varchar(255),
 	roles_id int references roles(id) unique
-);
-
-create table roles(
-	id serial primary key,
-	role_name varchar(255)
 );
 
 create table rules_list(
@@ -18,6 +18,16 @@ create table roles_rules(
 	id serial primary key,
 	roles_id int references roles(id),
 	rules_id int references rules_list(id)
+);
+
+create table states(
+	id serial primary key,
+	state varchar(255)
+);
+
+create table categories(
+	id serial primary key,
+	category_name varchar(255)
 );
 
 create table items(
@@ -39,16 +49,6 @@ create table attachs(
 	id serial primary key,
 	file_name varchar(255),
 	item_id int references items(id) unique
-);
-
-create table states(
-	id serial primary key,
-	state varchar(255)
-);
-
-create table categories(
-	id serial primary key,
-	category_name varchar(255)
 );
 
 
