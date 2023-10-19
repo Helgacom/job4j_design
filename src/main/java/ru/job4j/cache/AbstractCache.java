703b19f2ce19f8fad40dfe02c8aaed46817faa55
Olga Remneva <remneva_olga@mail.ru>
2023-10-19 11:11:59 +0400

    cache on SoftReference - fixed 2

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -6,14 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractCache<K, V> {
-    public AbstractCache() {
-    }
 
-    protected final Map<K, SoftReference<V>> cache = new HashMap<>();
+    private final Map<K, SoftReference<V>> cache = new HashMap<>();
 
-    public void put(K key, V value) {
-        SoftReference<V> reference = new SoftReference<>(value);
-        cache.put(key, reference);
+    public final void put(K key, V value) {
+        cache.put(key, new SoftReference<>(value));
     }
 
     public V get(K key) throws IOException {
